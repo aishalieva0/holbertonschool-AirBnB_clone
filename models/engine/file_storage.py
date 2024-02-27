608523b5f1ebc5f48b00ccb3530260589efa33d4
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-27 20:48:03 +0400

    soem fixes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,12 +21,15 @@ class FileStorage():
 
     def save(self):
         obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
+        print(obj_dict)
         with open(self.__file_path, "w") as file:
             json.dump(obj_dict, file)
 
     def reload(self):
+        data = {} 
         try:
             with open(self.__file_path, "r") as file:
-                self.__objects = json.load(file)
+                data = json.load(file)
+            FileStorage.__objects = data
         except FileNotFoundError:
             pass
