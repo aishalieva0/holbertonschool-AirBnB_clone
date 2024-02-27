de07e82386f729c688ca66bc3f5d4615c7228406
aishalieva0 <aysalieva6@gmal.com>
2024-02-27 03:57:27 -0800

    base model created

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel:
             self.__dict__['updated_at'] = datetime.now()
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] (<{self.id}>) <{self.__dict__}>"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
