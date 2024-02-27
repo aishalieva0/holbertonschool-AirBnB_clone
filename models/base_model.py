29cc17b5288acecb8d4aab5d7c3d01a91324a099
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-27 18:37:59 +0400

    save_reload_base_model functionalities was added

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -22,6 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __setattr__(self, name: str, value) -> None:
         self.__dict__[name] = value
@@ -33,6 +35,7 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         result = dict(self.__dict__)
@@ -40,3 +43,4 @@ class BaseModel:
         result["created_at"] = self.created_at.isoformat()
         result["updated_at"] = self.updated_at.isoformat()
         return result
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,9 +5,27 @@ and deserializes JSON file to instances
 """
 
 
+import json
+
+
 class FileStorage():
-    __file_path = ""
+    """File Storage"""
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        return self.__objects
 
+    def new(self, obj):
+        self.__objects[obj.__class__.__name__] = obj
+
+    def save(self):
+        with open(self.__file_path, "w") as file:
+            json.dump(self.__objects, file)
+
+    def reload(self):
+        try:
+            with open(self.__file_path, "r", encoding="utf-8") as file:
+                self.__objects = json.load(file)
+        except FileNotFoundError:
+            pass
