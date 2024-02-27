77742da2b9eecf6551308b1da2a0dd07bfefb81e
aishalieva0 <aysalieva6@gmal.com>
2024-02-27 15:42:25 -0800

    updated reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,7 +29,7 @@ class FileStorage():
         try:
             with open(self.__file_path, "r") as file:
                 data = json.load(file)
-            for k in data:
-                self.__objects[k] = BaseModel(**data[k])
+            for k, v in data.items():
+                self.__objects[k] = BaseModel(**v)
         except FileNotFoundError:
             pass
