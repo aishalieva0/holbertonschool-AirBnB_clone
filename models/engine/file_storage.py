0df048bac6938e5cd9051dadadffeac6ba74fd98
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-27 19:51:54 +0400

    stored obj

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,16 +17,16 @@ class FileStorage():
         return self.__objects
 
     def new(self, obj):
-        self.__objects[obj.__class__.__name__] = obj
+        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
-
+        obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
         with open(self.__file_path, "w") as file:
-            json.dump(self.__objects, file)
+            json.dump(obj_dict, file)
 
     def reload(self):
         try:
-            with open(self.__file_path, "r", encoding="utf-8") as file:
+            with open(self.__file_path, "r") as file:
                 self.__objects = json.load(file)
         except FileNotFoundError:
             pass
