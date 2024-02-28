6e023e8718a78afa20919aebcdddaabdc5d528f7
aishalieva0 <aysalieva6@gmal.com>
2024-02-28 03:26:59 -0800

    documentation added to console file

--- console.py
+++ console.py
@@ -10,19 +10,22 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """ commands """
     def do_quit(self, arg):
+        """ exit """
         return True
 
     def do_EOF(self, arg):
+        """ end of file """
         return True
 
     def do_help(self, arg):
+        """ help """
         print("help!")
 
     cmd.Cmd.prompt = '(hbnb)'
 
     def emptyline(self):
+        """ overrides emptyline """
         pass
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
