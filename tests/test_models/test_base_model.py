e2e504c79759ffdc116216ee52d9e6ee6292b2be
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-03-02 17:03:54 +0400

    first try baseModel test case

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,8 +5,28 @@ unittest for base module
 
 
 import unittest
+from models.base_model import BaseModel
+from os import path
 
 
 class Test_BaseModel(unittest.TestCase):
     """ test for BaseModel class """
+    def test_init(self):
+        obj = BaseModel()
+        self.assertIsNotNone(obj.id)
+        self.assertIsNotNone(obj.updated_at)
+        self.assertIsNotNone(obj.created_at)
 
+    def test_save(self):
+        obj = BaseModel()
+        obj.save()
+        self.assertTrue(path.isfile("file.json"))
+        with open('file.json') as f:
+            self.assertIn("BaseModel." + obj.id , f.read())
+
+    def test_str(self):
+        obj = BaseModel()
+        self.assertIn(obj.id, str(obj))
+
+if __name__ == '__main__':
+    unittest.main()
